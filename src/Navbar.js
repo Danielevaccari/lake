ec50373d2a02906b91f2a6b6871985e65e39fe3e
Daniele Vaccari <djvacc@utu.fi>
2021-06-02 12:38:56 +0300

    work in progress

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,11 +10,20 @@ function Navbar() {
 
                 <div className='right'>
                     <div className='links'>
-                        <a className='link' href='/#'>Men</a>
-                        <a className='link' href='/#'>Women</a>
+                        <a className='link' href='/#'>Log in</a>
+                        <a className='link' href='/#'>Create account</a>
                         <a className='link' href='/#'>Auditorial</a>
                         <a className='link' href='/#'>Contact Us</a>
                     </div>
+                </div>
+            </div>
+            {/*Second part of nav-bar*/}
+            <div className='categories'>
+                <div className='category'>
+                    <a className='link' href='/#'>Men</a>
+                    <a className='link' href='/#'>Women</a>
+                    <a className='link' href='/#'>Filter</a>
+                    <a className='link' href='/#'>SALE</a>
                 </div>
             </div>
         </>
