19fcad3e168fa3a4fa6733db7a436f82bf04a1fa
Daniele Vaccari <djvacc@utu.fi>
2021-06-18 09:06:03 +0300

    checkout process ready

--- src/Webpage.js
+++ src/Webpage.js
@@ -3,12 +3,15 @@ import Navbar from './Navbar'
 import Products from './Products/Products'
 import Cart from './Cart/Cart'
 import { commerce } from './library/commerce'
-import { BrowserRouter as Router, Switch, Route, HashRouter } from 'react-router-dom'
+import { Switch, Route, HashRouter } from 'react-router-dom'
+import Checkout from './Checkout/Checkout'
 
 function Webpage() {
 
     const [products, setProducts] = useState([])
     const [cart, setCart] = useState({})
+    const [order, setOrder] = useState({})
+    const [errorMessage, setErrorMessage] = useState('')
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list()
@@ -34,12 +37,23 @@ function Webpage() {
         const { cart } = await commerce.cart.empty()
         setCart(cart)
     }
+    const refresCart = async () => {
+        const newCart = await commerce.cart.refresh()
+        setCart(newCart)
+    }
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+        try {
+            const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder)
+            setOrder(incomingOrder)
+            refresCart()
+        } catch (error) {
+            
+        }
+    }
     useEffect(() => {
         fetchProducts()
         fetchCart()
     }, [])
-
-    console.log(cart)
 
     return (
         <>
@@ -54,6 +68,7 @@ function Webpage() {
                     </Route>
 
                     <Route exact path='/cart'>
+                        <Navbar totalItems={cart.total_items} />
                         <div className='web2'>
 
                             <Cart cart={cart}
@@ -61,6 +76,10 @@ function Webpage() {
                                 handleRemoveFromCart={handleRemoveFromCart}
                                 handleUpdateQuantity={handleUpdateQuantity} />
                         </div>
+                    </Route>
+                    <Route exact path='/checkout'>
+                        <Navbar totalItems={cart.total_items} />
+                        <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
                     </Route>
 
                 </Switch>
